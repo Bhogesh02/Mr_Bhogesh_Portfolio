70d804610addb9b6acea4f6f44a4bc6cf4464731
Bhogesh02 <tamminanabhogesh@gmail.com>
2024-12-21 22:16:53 +0530

    scrool up animation

--- src/components/scroll-top.jsx
+++ src/components/scroll-top.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { FaArrowUp } from "react-icons/fa6";
 
 const DEFAULT_BTN_CLS =
-  "fixed bottom-8 right-6 z-50 flex items-center rounded-full bg-gradient-to-r from-pink-500 to-violet-600 p-4 hover:text-xl transition-all duration-300 ease-out";
+  "fixed bottom-8 right-6 z-50 flex items-center rounded-full bg-gradient-to-r from-pink-500 to-violet-600 p-4 hover:text-xl transition-all duration-300 ease-out animate-bounce"; // Added animate-bounce here
 const SCROLL_THRESHOLD = 50;
 
 const ScrollToTop = () => {
@@ -28,7 +28,7 @@ const ScrollToTop = () => {
 
   return (
     <button className={btnCls} onClick={onClickBtn}>
-      <FaArrowUp />
+      <FaArrowUp className="w-6 h-6" />
     </button>
   );
 };
