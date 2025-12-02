1deaa8910cc0c98d43b05127550e63ed6beee1c9
Bhogesh02 <tamminanabhogesh@gmail.com>
2025-12-02 13:44:11 +0530

    yaml file added

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // required for GitHub Pages
-  images: {
-    unoptimized: true, // required (GitHub Pages doesn't support image optimization)
-  },
-  basePath: "/Mr_Bhogesh_Portfolio", // IMPORTANT: replace with your repo name
-  assetPrefix: "/Mr_Bhogesh_Portfolio/",
+  output: "export",
+  images: { unoptimized: true },
+  basePath: "",
+  assetPrefix: "",
 };
 
 module.exports = nextConfig;
